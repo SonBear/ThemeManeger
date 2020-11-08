693f0ff0576f9bfbd529f9ada268033e63024533
EusebioAjas <eusebioajas6@gmail.com>
2020-11-07 21:17:43 -0600

    experimentos

--- ThemesManeger/src/com/themesmaneger/maneger/ManegerTheme.java
+++ ThemesManeger/src/com/themesmaneger/maneger/ManegerTheme.java
@@ -13,11 +13,6 @@ import java.awt.Component;
 import java.awt.Container;
 import java.util.ArrayList;
 import java.util.List;
-import javax.swing.Box;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JTable;
-import javax.swing.JTextField;
 
 /**
  *
@@ -46,39 +41,16 @@ public class ManegerTheme {
     private boolean aplyTheme(Theme theme, Container window) {
         List<Component> components = getAllComponents(window);
 
-        for (Component comp : components) {
-            System.out.println(comp.getClass());
-
-            if (comp instanceof JPanel) {
-                JPanel panel = (JPanel) comp;
-                panel.setBackground(theme.COLOR_PANEL_BACKGROUND);
-                System.out.println("black");
-            } else if (comp instanceof JLabel) {
-                JLabel label = (JLabel) comp;
-                label.setOpaque(true);
-                label.setBackground(theme.COLOR_LABEL_BACKGROUND);
-                label.setForeground(theme.COLOR_LABEL_FOREGROUND);
-            } else if (comp instanceof Box.Filler) {
-                Box.Filler filler = (Box.Filler) comp;
-                filler.setOpaque(true);
-                filler.setBackground(theme.COLOR_PANEL_BACKGROUND);
-            } else if (comp instanceof JTextField) {
-                JTextField text = (JTextField) comp;
-                text.setOpaque(true);
-                text.setBackground(theme.COLOR_TEXTFIELD_BACKGROUND);
-                text.setForeground(theme.COLOR_TEXTFIELD_FOREGROUND);
-            } else if (comp instanceof JTable) {
-                JTable table = (JTable) comp;
-                table.setForeground(theme.COLOR_TABLE_FOREGROUND);
-                table.setBackground(theme.COLOR_TABLE_BACKGROUND);
-            }
-        }
+        components.forEach(component -> {
+            component.setBackground(theme.COLOR_FILLER_BACKGROUND);
+            component.setForeground(theme.COLOR_LABEL_FOREGROUND);
+        });
 
         return true;
     }
 
-    private List<Component> getAllComponents(final Container c) {
-        Component[] comps = c.getComponents();
+    private List<Component> getAllComponents(final Container container) {
+        Component[] comps = container.getComponents();
         List<Component> compList = new ArrayList<Component>();
         for (Component comp : comps) {
             compList.add(comp);
